e48afb35a52b1e2e9aea8005eb6fab766dbcb0fc
kateosipenko <kate.osipenko@gmail.com>
2014-07-31 14:49:59 +0300

    Added bindable app bar, phone toolkit; localization logic and styles; updated home page

---
+++ WhatYouEatWP7/Helpers/SystemTrayBehavior.cs
@@ -0,0 +1,54 @@
+﻿namespace WhatYouEatWP7.Helpers
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Interactivity;
+    using System.Windows.Media;
+    using System.Windows.Threading;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+
+    /// <summary>
+    /// Behavior for dislpaying system tray for 3 seconds by flip.
+    /// </summary>
+    public class SystemTrayBehavior : Behavior<UIElement>
+    {
+        private const int SystemTrayVisibilityDutration = 3000;
+
+        private static DispatcherTimer timer;
+
+        static SystemTrayBehavior()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(SystemTrayVisibilityDutration);
+            timer.Tick += OnTimerTick;
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.MouseMove += this.AssociatedObject_MouseMove;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.MouseMove -= this.AssociatedObject_MouseMove;
+            base.OnDetaching();
+        }
+
+        private static void OnTimerTick(object sender, EventArgs e)
+        {
+            SystemTray.IsVisible = false;
+            timer.Stop();
+        }
+
+        private void AssociatedObject_MouseMove(object sender, MouseEventArgs e)
+        {
+            SystemTray.Opacity = 0;
+            SystemTray.ForegroundColor = (Color)Application.Current.Resources["PhoneForegroundColor"];
+            SystemTray.IsVisible = true;
+            timer.Start();
+        }
+    }
+}
---
+++ WhatYouEatWP7/Resources/LocalizationManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using WhatYouEatWP7.Resources.Buttons;
+using WhatYouEatWP7.Resources.Common;
+
+namespace WhatYouEatWP7.Resources
+{
+    public class LocalizationManager : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private CommonStrings commonStrings = new CommonStrings();
+        private ButtonsStrings buttonStrings = new ButtonsStrings();
+
+        #endregion Fields
+
+        #region Properties
+
+        public CommonStrings Common
+        {
+            get { return commonStrings; }
+        }
+
+        public ButtonsStrings Buttons
+        {
+            get { return buttonStrings; }
+        }
+
+        #endregion Properties
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void RaiseLocalizationChanged()
+        {
+            RaisePropertyChanged("Common");
+            RaisePropertyChanged("Buttons");
+        }
+
+        #endregion INotifyPropertyChanged
+
+    }
+}
